7af7891b84da1e0a658cb36ac2c751a1a65b8c04
YegorFrolov <dmtyegor@gmail.com>
2022-03-23 17:04:06 +0300

    Задача027

---
+++ Задача027/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+/*
+Нашел в тырнете
+int result = 452.ToString().Sum(c => c - '0');
+Console.WriteLine(result);
+Как работает не понятно =/
+*/
+
+/*
+Вариант 1
+Console.WriteLine("Введите любое число: ");
+string numstr = Convert.ToString(Console.ReadLine());
+int sum = 0;
+sum = numstr.Sum(i => i - '0');
+Console.WriteLine(sum);
+*/
+
+Console.WriteLine("Введите любое число: ");
+string num = Convert.ToString(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i < num.Length; i++)
+{
+    int number = Convert.ToInt32(num[i].ToString());
+    sum += number;
+}
+Console.WriteLine(sum);
