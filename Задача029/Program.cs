50d39f990e700cdecea6f82d90f76cc8f49dc007
YegorFrolov <dmtyegor@gmail.com>
2022-03-23 17:35:39 +0300

    Задача029

---
+++ Задача029/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из N(-100 100) рандомных элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Вариант 1
+Console.WriteLine("Введите кол-во символов в массиве");
+int size = Convert.ToInt32(Console.ReadLine());
+int newNumber = 0;
+for (int i = 0; i < size; i++)
+{
+   newNumber = new Random().Next(-100,100);
+   Console.Write($"{newNumber}, ");
+}
