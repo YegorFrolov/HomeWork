949a61c1a653d9810521acd60b9a7436c9555a1e
YegorFrolov <dmtyegor@gmail.com>
2022-04-09 13:20:39 +0300

    62

--- Задача062/Program.cs
+++ Задача062/Program.cs
@@ -4,25 +4,30 @@
 
 
 Console.Clear();
-Console.Write("Введите размеры квадратной матрицы: ");
-int n = Convert.ToInt32(Console.ReadLine()); // Количество строк, столбцов массива n x n
+Console.Write("Введите размер квадратной матрицы ");
+int row = Convert.ToInt32(Console.ReadLine()); 
+
+int column = row; 
+
 int number = 1; // первый элемент массива
 
-Print2(SpiralArray(n, number));
+Console.WriteLine(string.Empty);
+
+Print2(SpiralArray(row, column, number));
 
 Console.WriteLine("\n"); Console.ReadKey();
 
-static int[,] SpiralArray(int n, int number)
+static int[,] SpiralArray(int row, int column, int number)
 {
-int[,]array = new int[n,n];
+int[,]array = new int[row,column];
 int count = 0; // счётчик пройденных витков спирали
-int row; int column; // строка и столбец текущего элемента массива
-while (number < n*n + 1)
+int rownow; int columnnow; // строка и столбец текущего элемента массива
+while (number < row*column + 1)
 {
-    for (column = count; column < n - count; column++) array[count, column] = number++; // верхняя горизонталь витка спирали, слева направо
-    for (row = count + 1; row < n - count; row++) array[row, n - count - 1] = number++; // правая вертикаль витка спирали, сверху вниз
-    for (column = n - count - 2; column > count; column--) array[n - count - 1, column] = number++; // нижняя горизонталь витка спирали, справа налево
-    for (row = n - count - 1; row > count; row--) array[row,count] = number++; // левая вертикаль витка спирали, снизу вверх
+    for (columnnow = count; columnnow < column - count; columnnow++) array[count, columnnow] = number++; // верхняя горизонталь витка спирали, слева направо
+    for (rownow = count + 1; rownow < row - count; rownow++) array[rownow, row - count - 1] = number++; // правая вертикаль витка спирали, сверху вниз
+    for (columnnow = column - count - 2; columnnow > count; columnnow--) array[column - count - 1, columnnow] = number++; // нижняя горизонталь витка спирали, справа налево
+    for (rownow = row - count - 1; rownow > count; rownow--) array[rownow,count] = number++; // левая вертикаль витка спирали, снизу вверх
 count++;
 }
 return array;
